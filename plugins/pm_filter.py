2ebc28a2ec9f0b2f8f14b9f82518a635cb6620a8
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-12-19 22:35:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -48,8 +48,6 @@ async def group_search(client, message):
             m = await message.reply_sticker("CAACAgUAAxkBAAKRAWdkPvaCHT5t0PaVGssxbSx660MaAAJdAAM7SrMpdt9JqhNVd-82BA")
             await asyncio.sleep(1)
             await m.delete()
-            except Exception as e:
-                logger.warning(f"Error deleting message: {e}")
             return await message.reply('<b>‼️ ᴡʜʏ ʏᴏᴜ ꜱᴇɴᴅ ʜᴇʀᴇ ʟɪɴᴋ\nʟɪɴᴋ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ʜᴇʀᴇ 🚫</b>')
 
         elif '@admin' in message.text.lower() or '@admins' in message.text.lower():
